d20fb344d99953bc5583a14833d771b1fbe8cfbf
Anderson Lomba <60937513+LombaAnderson@users.noreply.github.com>
2022-02-16 02:52:06 -0300

    Update App.tsx

--- cadastro/src/App.tsx
+++ cadastro/src/App.tsx
@@ -1,33 +1,14 @@
 import { Router } from './router';
 import { FormProvider } from './contexts/FormContext';
-import React, {Component, useEffect, useState} from 'react';
-import axios from 'axios';
+import io from 'socket.io-client';
 
-
-
+io('http://localhost:3000');
 const App = () => {
-
-  useEffect(() =>{
-    axios.get("http://localhost:3000/usuario/listar")
-   .then(() => {
-     console.log("Tudo ok!")
-
-   }).catch(() =>{
-     console.log("Falha!")
-
-   })
-  
-  }, [])
-  
   return (
-
     <FormProvider>
-          
       <Router />
-      
     </FormProvider>
   );
-  
 }
 
 export default App;
